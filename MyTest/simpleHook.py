7b168ef13e930c4924e83b36e95810267b1d9254
Envy <326556606@qq.com>
2018-07-10 23:36:42 +0800

    deal simpleHook
    deal simpleHook

--- MyTest/simpleHook.py
+++ MyTest/simpleHook.py
@@ -4,6 +4,8 @@
 # @editTool	sublime
 # @time		2018-7-9 23:51:23
 # 钩子(Hook) 类似于回调、注册，将hook函数注册、或者是类似与监听之类的到目标点上
+# 说一下 hook 的机制：
+#			对要扩展（暂时不扩展）的地方放置一个钩子函数，即对象本体需要有接口对接
 
 class Student(object):
 	"""a classes class Student"""
@@ -18,24 +20,21 @@ class Student(object):
 	def study(self):
 		print("student ",self.name," is studying...")
 				
-	def stuRun(self):
-		import time
-		import random
-		while 1:
-			time.sleep(3)
-			if random.randint(1,2) == 1:
-				self.goHome()
-				pass
-			else:
-				self.study()
+	
 
 class Student_Hooker(object):
 	"""
 		a content some hook function class
 	"""
 	def __init__(self):
-		super(Hooker, self).__init__()
 		self.student_hook = False
+		# 对实例对象的方法hook挂载
+		self.student_gohome_hook = False
+		self.student_study_hook = False
+
+	def getStudent(self):
+		if self.student_hook:
+			return self.student
 
 	def setStudent(self, student):
 		if type(student)==Student:
@@ -43,17 +42,64 @@ class Student_Hooker(object):
 			self.student = student
 			self.student_hook = True
 
-	# 外界通过 register 获取设置内部方法
-	student_register = property(self.student = student)
-	
-	def func_hook(self):
-		
+	def getGoHome(self):
+		if self.student_gohome_hook:
+			return self.student_gohome_hook_func
 
-	# 注册器提供注册接口
-	# def register_goHome_hook(self, func_goHome):
-	# 	pass
+	# 设置挂载点
+	def setGoHome(self, hook_func, *args, **kwargs):
+		if self.student_hook is False:
+			return;
+		self.student_gohome_hook_func = hook_func
+		self.student_gohome_hook = True
+
+	def getStudy(self):
+		if self.student_study_hook:
+			return self.student_study_hook_func
+
+	def setStudy(self, hook_func, *args, **kwargs):
+		if self.student_hook is False:
+			return;
+		self.student_study_hook_func = hook_func
+		self.student_study_hook = True
+	
+	# 外界通过 register 获取设置内部方法
+	# 使用方法： hooker实例.student_register = Student实例
+	student_register = property(getStudent, setStudent)
+	student_gohome_register = property(getGoHome, setGoHome)
+	student_study_register = property(getStudy, setStudy)
+
+	def stuRun(self):
+		import time
+		import random
+		while 1:
+			time.sleep(3)
+			if random.randint(1,2) == 1:
+				self.student.goHome()
+				if self.student_gohome_hook:
+					self.student_gohome_hook_func()
+				pass
+			else:
+				self.student.study()
+				if self.student_study_hook:
+					self.student_study_hook_func()
+			
+def my_gohome_plugin():
+	print("let's go home!!!")
+
+def my_study_plugin():
+	print("let's study!!!")
+
 
 def main():
 	stu = Student("Lili", 123)
 	shooker = Student_Hooker()
 	shooker.student_register = stu
+	# 可以挂载自定义函数,差不多就是这个样，写的太复杂了些
+	shooker.student_gohome_register = my_gohome_plugin
+	shooker.student_study_register = my_study_plugin
+	# 执行在 Hooker 上
+	shooker.stuRun()
+
+if __name__ == '__main__':
+	main()
