6f6bb8714337df6827a1b695b47e04ee78a92459
_(:зゝ∠)_biubiu~~ <326556606@qq.com>
2018-07-14 11:31:11 +0800

    deal a little problem
    deal a little problem

--- MonkeySamplify/finish/Commons.py
+++ MonkeySamplify/finish/Commons.py
@@ -4,9 +4,9 @@ def getDeviceList():
 	from os import popen
 	serialno_list = []
 	with popen("adb devices") as devices:
-	readStr = devices.read()
-	tmplist = readStr.strip().split("\n")
-	tmplist.pop(0)
+		readStr = devices.read()
+		tmplist = readStr.strip().split("\n")
+		tmplist.pop(0)
 	for x in tmplist:
 		index = x.index('\t');
 		serialno_list.append(x[0:index])
