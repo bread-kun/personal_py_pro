23fc47f97794e504a81dc8cd4cda39fb2d980843
_(:зゝ∠)_biubiu~~ <326556606@qq.com>
2018-08-09 17:25:25 +0800

    deal a little problem
    sort and generate new json file

--- json/operate_json.py
+++ json/operate_json.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # a little demo that operate json file in python
+# @author asange
+# @time 2018-8-8 09:22:26
 
 import json
 import sys
@@ -14,6 +16,10 @@ def __check_file(args = sys.argv):
 		else:
 			print("%s is in not exist" %_file)
 
+# defult in current path
+# @param _decoder  coding for read file
+# @param _path 		current page file path, defult plus signal "./"
+# @return  load_dict the file json with dict type
 def read_json(_path, _decoder = "utf-8"):
 	# check file is exist or not
 	if os.path.isfile("./%s" %_path):
@@ -21,7 +27,7 @@ def read_json(_path, _decoder = "utf-8"):
 		try:
 			f = open('./%s' %_file, "r", encoding=_decoder)
 			load_dict = json.load(f)
-			print(load_dict)
+			return load_dict
 		except UnicodeDecodeError as unicode_e:
 			# if the target file is encode by gbk , here can auto chage to the gbk but only in this
 			if _decoder == "utf-8":
@@ -29,20 +35,44 @@ def read_json(_path, _decoder = "utf-8"):
 			else:
 				raise UnicodeDecodeError("未知文件编码", unicode_e)
 		except json.decoder.JSONDecodeError as json_e:
-			raise json.decoder.JSONDecodeError("JSON 文件格式错误", json_e)
+			print("JSON 文件格式错误,请检查文件格式是否有误")
+			raise json_e
 		except Exception as e:
 			raise e
-		else:
-			pass
 		finally:
 			f.close()
 	else:
 		print("param error!")
 
+# @param dict 	recive a dict type param to generate a new file which have order by first level dict key
+# @return scussee or not
+def sort_json(json_dict):
+	if len(json_dict) < 1 or type(json_dict) != dict:
+		raise Exception("文件为空")
+	_dict = {}
+	_list = list(json_dict.keys())
+	# warning : sort() have no return any value
+	_list.sort()
+	try:
+		# order dict
+		for _k in _list:
+			_dict[_k] = json_dict[_k]
+		_f = open('./sorted_json.json',"w+", encoding="utf-8")
+		json.dump(_dict, _f)
+	except Exception as e:
+		raise e
+	finally:
+		_f.close()
+		pass
+		
+
 
 def main(args = sys.argv):
 	if len(args) > 1:
-		read_json(_path = args[1])
+		load_dict = read_json(_path = args[1])
+		sort_json(load_dict)
+		# print(load_dict["wifi"][0]["account"])
+		# print(load_dict["wifi"][0]["password"])
 	else:
 		raise Exception("commond error: need one more param")
 	
